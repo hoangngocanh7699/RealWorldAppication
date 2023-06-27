59fdd9a7c98aa7c33c009cc19edb06d1df97e409
hoangngocanh7699 <62314300+hoangngocanh7699@users.noreply.github.com>
2023-06-27 12:48:02 +0700

    push code

---
+++ src/component/todos/todos.js
@@ -0,0 +1,11 @@
+
+function Todos() {
+    return (
+      <div className="App">
+        123
+      </div>
+    );
+  }
+  
+  export default Todos
+  
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Todos from './component/todos/todos';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Todos />
 );
 
 // If you want to start measuring performance in your app, pass a function
